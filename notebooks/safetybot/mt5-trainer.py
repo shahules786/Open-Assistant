3b5ded225d71277b9832fe762dea3e4adca1a973
Shahules786 <shahules786@gmail.com>
2023-03-13 05:14:32 +0000

    config

--- notebooks/safetybot/mt5-trainer.py
+++ notebooks/safetybot/mt5-trainer.py
@@ -47,15 +47,16 @@ wandb.login(key=wandb_key)
 CONFIG = {"special_tokens":SPECIAL_TOKENS,
 "model":"t5-base",
 "max_len":256,
-"gradient_accumulation_steps":1,
-"epochs":1,
-"batch_size":8,
+"gradient_accumulation_steps":8,
+"epochs":2,
+"batch_size":2,
 "fp16":False,
 "train_dataset":"allenai/prosocial-dialog",
 "Notes":"MT5 using prosocial",
 "train_dataset":{
     "allenai/prosocial-dialog":["train","validation"],
     "shahules786/prosocial-confessions":"train",
+     "shahules786/prosocial_augmented":"train",
 
 },
 "test_dataset":{"allenai/prosocial-dialog":"test"},
@@ -257,7 +258,7 @@ if __name__ == "__main__":
         json.dump(CONFIG,file,indent=4)
     
 
-    model = T5ForConditionalGeneration.from_pretrained(CONFIG["model"])
+    model = MT5ForConditionalGeneration.from_pretrained(CONFIG["model"])
     tokenizer = AutoTokenizer.from_pretrained(CONFIG["model"],padding_side="right",truncation_side="right",model_max_length=512)
     add_special_tokens(tokenizer,model)
     
